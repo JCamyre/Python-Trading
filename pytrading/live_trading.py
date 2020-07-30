884778e55a4e12079ccbbb5156ee2bf0c790821d
JCamyre <JWcamry03@gmail.com>
2020-07-29 17:13:25 -0700

    Deleting many unnecessary comments

--- pytrading/live_trading.py
+++ pytrading/live_trading.py
@@ -22,9 +22,6 @@ pd.options.display.max_columns = 10
 
 my_stocks = ['AGI', 'AMAT', 'AYRO', 'BIDU', 'BILI', 'BILL', 'JBLU', 'JD', 'KHC', 'LUV', 'QD', 'RGR', 'SWBI', 'TSM', 'VIPS']
 
-# with open('literally_all_tickers', 'rb') as f:
-# 	tickers_to_check = pickle.load(f)
-
 def alert_sound():
 	regular_instrumental = mixer.music.load("C:/Users/JWcam/Desktop/NCT 127 - Regular (Instrumental).mp3")
 	mixer.music.set_volume(0.7)
@@ -33,19 +30,8 @@ def alert_sound():
 	mixer.music.stop()
 
 
-'''Getting premarket movers -> reading charts -> getting list of 5-10 interesting stocks -> drawing/writing SR levels -> strategies with 
-enter/exit prices -> be dynamic, sometimes things do not go to plan (breakout or plummet) -> stop losses
-
-If a stock goes through resistance, but falls back on resistance can be either acting as support or could just fall through'''
-
-# Get low float stocks. High short interest
-# Look for super basing penny stocks/RSI low
-# Day 3/4 Penny stocks
-
-
 
 def stock_tracker(portfolio):
-	# iNSTEAD OF SEARCHING FOR ALL STOCKS FOR THE MID DAY MOVERS, JUST TAKE ALL STOCKS UP MORE THAN A COUPLE PERCENT WITH SOME EITHER PENNY STOCK CRITERIA, THEN JUST SEARCH THROUGH THOSE
 	t1 = time()
 	info = [f"Time: {strftime('%I:%M:%S', localtime())}"]
 	for stock in portfolio:
@@ -76,23 +62,10 @@ def trending_stocks(portfolio):
 	t1 = time()
 	for stock in portfolio:
 		stock.update_stock()
-
-		# try:
-		# 	cur_stats = stock.df_month.iloc[-1]
-		# 	prev_stats = stock.df_month.iloc[-2]
-		# except:
-		# 	print(stock.ticker, 'this one bad.')
-		# 	continue
-
 		intra_day_stats = stock.df
 		m1, m2, m15, m30 = intra_day_stats.iloc[-2:-1][['Volume', 'Close']], intra_day_stats.iloc[-3:-1][['Volume', 'Close']]/2, intra_day_stats.iloc[-16:-1][['Volume', 'Close']]/15, intra_day_stats.iloc[-31:-1][['Volume', 'Close']]/30
-		
-		# delta_volume = cur_stats['Volume']/sum(stock.df_month.iloc[-30:]['Volume'])
-		# change_percentage = ((cur_stats['Close'] - prev_stats['Close'])/prev_stats['Close'])*100 
 
-		# and delta_volume > 1.25 Is this needed?
 		# Maybe detect a drop in value as well?
-		# Maybe drop one minute volume/close
 		if m2['9_sma'] < sum(m2['close']) or sum(m2['Volume']) > (1.25 * sum(m15['Volume'])) and sum(m2['Close']) > (1.015 * sum(m15['Close'])) and cur_stats['Volume'] > 100_000:
 			stocks_to_display.append(stock.ticker + ' 2m')
 		elif m1['9_sma'] < sum(m1['close']) or sum(m1['Volume']) > (1.25 * sum(m15['Volume'])) and sum(m1['Close']) > (1.015 * sum(m15['Close'])) and cur_stats['Volume'] > 100_000:
@@ -100,52 +73,3 @@ def trending_stocks(portfolio):
 	print(f'Computing time: {time() - t1:.2f}s. Average of {len(portfolio)/(time() - t1):.2f} stocks per second.')	
 	return stocks_to_display
 
-# def trending_stocks(portfolio, repeat=False):
-# 	def return_trending_stocks(portfolio):
-# 		stocks_to_display = []
-# 		t1 = time()
-# 		for stock in portfolio:
-# 			stock.update_stock()
-
-# 			try:
-# 				cur_stats = stock.df_month.iloc[-1]
-# 				prev_stats = stock.df_month.iloc[-2]
-# 			except:
-# 				print(stock.ticker, 'this one bad.')
-# 				continue
-
-# 			intra_day_stats = stock.df
-# 			m1, m3, m15, m30 = intra_day_stats.iloc[-2:-1][['Volume', 'Close']], intra_day_stats.iloc[-4:-1][['Volume', 'Close']]/3, intra_day_stats.iloc[-16:-1][['Volume', 'Close']]/15, intra_day_stats.iloc[-31:-1][['Volume', 'Close']]/30
-			
-# 			delta_volume = cur_stats['Volume']/sum(stock.df_month.iloc[-30:]['Volume'])
-# 			change_percentage = ((cur_stats['Close'] - prev_stats['Close'])/prev_stats['Close'])*100 
-
-# 			# and delta_volume > 1.25 Is this needed?
-# 			# Maybe detect a drop in value as well?
-# 			if sum(m3['Volume']) > (1.25 * sum(m15['Volume'])) or sum(m3['Close']) > (1.02 * sum(m15['Close'])) and cur_stats['Volume'] > 100_000:
-# 				stocks_to_display.append(stock.ticker)
-
-# 		print(f'Computing time: {time() - t1:.2f}s. Average of {len(portfolio)/(time() - t1):.2f} stocks per second.')	
-
-# 	if repeat:
-# 		try:
-# 			while True:
-# 				return_trending_stocks(portfolio)
-# 		except KeyboardInterrupt:
-# 			pass
-# 	else:
-# 		return_trending_stocks(portfolio)
-
-def intraday_stocks():
-	try:
-		while True:
-			print(f"Time: {strftime('%I:%M:%S', localtime())}", end='\n' * 2)
-			stock_tracker(day_tickers)
-			sleep(20)
-	except KeyboardInterrupt:
-		pass
-
-# BUY SOME AT 10, 11, 12, SELL SOME AT 15, BUY SOME AT 13 14 15, SELL SOME AT 18, ....
-# Buying trending stocks a few days later, if the chart looks constructive and with support. Has to drop a lot hto.
-# Ascending/descending triangle. Ascending: High lows, fighting resistance level, breakout. Descending: Lower highs, fighting support level, drop.
-# Stop loss 1 ATR? Low float stocks
