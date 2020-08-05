5b9286fe8951b6a2551923d40a48c6f4d65501e7
JCamyre <JWcamry03@gmail.com>
2020-08-05 08:47:36 -0700

    Added updatedd intraday tracker

---
+++ intra_day_tracker.py
@@ -0,0 +1,24 @@
+from pytrading import Portfolio
+from pytrading.live_trading import trending_stocks, stock_tracker
+from pytrading.download_tickers import load_biggest_movers, load_positions, pickle_biggest_movers, pickle_positions, get_day_hot_stocks
+from time import sleep
+
+def live_stock_data(*positions):
+	pickle_biggest_movers(Portfolio(get_day_hot_stocks()[:51]))
+	pickle_positions(Portfolio(positions))
+	i = 0
+	try:
+		while True:
+			if i % 15 == 0:
+				# If stock delisted, REMOVE IT FROM POSITION OBJECT
+				# Ok: so before we run the following line of code, we must loop through the new Portfolio, and if the element gives us an error,
+				# remove it. Use range(len(Portfolio())). Also, will have to pickle_biggest_movers() again.
+				# Or not? Maybe yahoo finance just buggy
+				pickle_biggest_movers(Portfolio(get_day_hot_stocks()[:51]))
+			trending_stocks(load_biggest_movers())
+			print(end='\n'*2)
+			stock_tracker(load_positions())
+			sleep(52)
+	except KeyboardInterrupt:
+		return
+
