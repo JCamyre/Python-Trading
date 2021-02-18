46510175e987b2a111c58cb56b0e44687ed858f9
JCamyre <JWcamry03@gmail.com>
2021-02-17 21:20:45 -0800

    Finished updating.

--- pytrading/setup.py
+++ pytrading/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyTrading", # Replace with your own username
+    name="Python-Trading", # Replace with your own username
     version="0.0.1",
     author="Joseph Camyre",
     author_email="jwcamry03@gmail.com",
