e743055266c954ce17904904cb49c775b2283a8a
JCamyre <JWcamry03@gmail.com>
2021-06-25 20:45:59 -0700

    Hid files in .gitignore.

--- stock_testing.py
+++
@@ -1,5 +0,0 @@
-from pytrading import Portfolio
-for stock in Portfolio(['WHLM', 'ARLO']):
-	print(stock.df['High'].iloc[-250:-100])
-	print(stock.df['Close'].iloc[-250:-100].idxmax())
-
--- testing.py
+++
@@ -1,6 +0,0 @@
-from intra_day_tracker import live_stock_data
-from pytrading import Portfolio
-# live_stock_data()
-
-test_portfolio = Portfolio(['TTM', 'ADTX'], '1m', '1d')
-print([stock.df for stock in test_portfolio])
